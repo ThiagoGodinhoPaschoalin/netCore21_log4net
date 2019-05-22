a2d36c58c800e65a6d92dead5df7df1b6f22fd71
thiago@conexia <thiago.godinho@conexia.com.br>
2019-05-22 16:00:49 -0300

    using 2 distinct logs;

---
+++ ConsoleApp_manyLogs/Base_Log4Net_XmlConfigurator.cs
@@ -0,0 +1,36 @@
+﻿using log4net;
+using log4net.Config;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Xml;
+
+namespace ConsoleApp_manyLogs
+{
+    public class BaseLog4NetXmlConfigurator
+    {
+        public BaseLog4NetXmlConfigurator()
+        {
+            //-> Para adicionar o arquivo de config ao assembly compilado:
+            //  Open SolutionExplorer -> ... -> log4net.config -> (RightButton) Properties -> BuildAction = "Embedded resource"
+            // Now you find the config in your assembly file
+            string log4netAssemblyName = Assembly
+                .GetExecutingAssembly()
+                .GetManifestResourceNames()
+                .FirstOrDefault(name => name.Contains("log4net.config"));
+
+            Stream log4netStream = Assembly.GetExecutingAssembly().GetManifestResourceStream(log4netAssemblyName);
+
+            XmlDocument log4netConfig = new XmlDocument();
+
+            log4netConfig.Load(log4netStream);
+
+            var repo = LogManager.CreateRepository(
+                Assembly.GetEntryAssembly(), typeof(log4net.Repository.Hierarchy.Hierarchy));
+
+            System.Console.WriteLine(repo.Name);
+
+            XmlConfigurator.Configure(repo, log4netConfig["log4net"]);//tag no xml! <log4net>...</tag4net>
+        }
+    }
+}
---
+++ ConsoleApp_manyLogs/Program.cs
@@ -0,0 +1,36 @@
+﻿using log4net;
+using log4net.Config;
+using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Xml;
+
+namespace ConsoleApp_manyLogs
+{
+    class Program
+    {
+        /// <summary>
+        /// New instance for log
+        /// </summary>
+        private static readonly ILog logA = LogManager.GetLogger(Assembly.GetExecutingAssembly(), "Logger_Sample_A");
+        private static readonly ILog logB = LogManager.GetLogger(Assembly.GetExecutingAssembly(), "Logger_Sample_B");
+
+        static void Main(string[] args)
+        {
+            new BaseLog4NetXmlConfigurator();
+
+            logA.Debug("desenvolvimento");
+            logA.Warn("atenção!");
+            logA.Info("informção");
+            logA.Error("problema!");
+            logA.Fatal("vixee");
+
+            logB.Debug("desenvolvimento");
+            logB.Warn("atenção!");
+            logB.Info("informção");
+            logB.Error("problema!");
+            logB.Fatal("vixee");
+        }
+    }
+}
